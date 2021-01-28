5ce0c5942cb5b76595a602dfdb60d9533f640a50
Arachne <yu843377627@163.com>
2021-01-28 19:46:54 +0800

    first commit

---
+++ src/main/java/com/azkaban/azkabanResponse/FetchAllProjectsResponse.java
@@ -0,0 +1,21 @@
+package com.azkaban.azkabanResponse;
+
+import java.util.List;
+
+/**
+ * Created by shirukai on 2019-06-04 09:53
+ * 查询所有项目的响应
+ */
+public class FetchAllProjectsResponse extends BaseResponse {
+    private List<Project> projects;
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
+    }
+
+}
+
---
+++ src/main/java/com/azkaban/azkabanResponse/Flow.java
@@ -0,0 +1,13 @@
+package com.azkaban.azkabanResponse;
+
+public class Flow{
+    private String flowId;
+
+    public String getFlowId() {
+        return flowId;
+    }
+
+    public void setFlowId(String flowId) {
+        this.flowId = flowId;
+    }
+}
